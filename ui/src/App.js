365208f38b75db86c690de144a17577a42c3f0f8
Elmer McElmeel <mcelmee20@gmail.com>
2024-08-17 20:45:30 -0500

    second commit

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-import resumeA from './Resume-EJ.pdf'
+import resumeA from './ResumeA.jpg';
+import resumeB from './ResumeB.jpg';
 
 function App() {
   return (
@@ -12,16 +13,14 @@ function App() {
           <p>Hello Principal, I am Elmer McElmeel.</p>
           <p>This is a website made with React.js hosted by AWS</p>
           <p>I decided to throw this together on 08/17/2024, to help show some additional React and AWS knowledge</p>
-      </div>
-      <div>
-        <a href="https://drive.google.com/file/d/1-l_Vs4OI5tIwRETorZtrJS75Cl_VGnJ4/view?usp=sharing" class="App-link">Resume On Google Docs</a>
-      </div>
-      <object class="pdf" 
-        data=
-        {resumeA}
-        width="800"
-        height="900">
-      </object>
+        </div>
+        <div>
+          <a href="https://drive.google.com/file/d/1-l_Vs4OI5tIwRETorZtrJS75Cl_VGnJ4/view?usp=sharing" class="App-link">Resume On Google Docs</a>
+        </div>
+          <img src={resumeA} alt="ResumeA" width="800" height="900" />
+          <div>
+            <img src={resumeB} alt="ResumeB" width="800" height="900" />
+          </div>     
       </div>
     </div>
   );
